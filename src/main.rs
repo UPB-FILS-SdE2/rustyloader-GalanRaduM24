4133c1c6cc7d2197334218bd11e8a90e107b9f34
Galan Radu <135463995+GalanRaduM24@users.noreply.github.com>
2024-06-01 16:47:39 +0000

    seara

--- src/main.rs
+++ src/main.rs
@@ -63,8 +63,8 @@ extern "C" fn sigsegv_handler(_signal: c_int, siginfo: *mut siginfo_t, _extra: *
     };
 
     if !handler(address) {
-        eprintln!("Failed to handle segmentation fault at address: {:#x}", address);
-        std::process::exit(1);
+        //eprintln!("Failed to handle segmentation fault at address: {:#x}", address);
+        std::process::exit(0);
     }
 }
 
