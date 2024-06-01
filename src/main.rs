7efe260a92782fa9e28d3d6d4561779a4b1036d6
Galan Radu <135463995+GalanRaduM24@users.noreply.github.com>
2024-06-01 11:32:20 +0000

    se intampla cevaaaaa

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ use nix::unistd::SysconfVar;
 mod runner;
 
 static mut SEGMENTS: Vec<(u64, u64, u64, u64, u64, object::SegmentFlags)> = Vec::new();
-static mut EXIT_CODE: i32 = 0;
 
 extern "C" fn sigsegv_handler(_signal: c_int, siginfo: *mut siginfo_t, _extra: *mut c_void) {
     let address = unsafe { (*siginfo).si_addr() } as usize;
@@ -41,9 +40,7 @@ extern "C" fn sigsegv_handler(_signal: c_int, siginfo: *mut siginfo_t, _extra: *
         }
     }
 
-    unsafe {
-        std::process::exit(EXIT_CODE);
-    }
+    std::process::exit(0);
 }
 
 fn segment_flags_to_prot_flags(flags: object::SegmentFlags) -> ProtFlags {
@@ -139,11 +136,7 @@ fn register_sigsegv_handler() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn exec(filename: &str, exit_code: i32) -> Result<(), Box<dyn Error>> {
-    unsafe {
-        EXIT_CODE = exit_code;
-    }
-
+fn exec(filename: &str) -> Result<(), Box<dyn Error>> {
     let segments = read_segments(filename)?;
     print_segments(&segments);
 
@@ -162,14 +155,12 @@ fn exec(filename: &str, exit_code: i32) -> Result<(), Box<dyn Error>> {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() != 3 {
-        eprintln!("Usage: {} <path-to-executable> <exit-code>", args[0]);
+    if args.len() != 2 {
+        eprintln!("Usage: {} <path-to-executable>", args[0]);
         std::process::exit(1);
     }
 
-    let exit_code: i32 = args[2].parse().unwrap_or(0);
-
-    exec(&args[1], exit_code)?;
+    exec(&args[1])?;
 
     Ok(())
 }
