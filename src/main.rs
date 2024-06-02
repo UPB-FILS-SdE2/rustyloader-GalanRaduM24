77d577546f128882e75ca32453923819ee0ab959
Galan Radu <135463995+GalanRaduM24@users.noreply.github.com>
2024-06-02 17:02:22 +0000

    aia e

--- src/main.rs
+++ src/main.rs
@@ -97,8 +97,8 @@ fn segment_flags_to_prot_flags(flags: object::SegmentFlags) -> ProtFlags {
 }
 
 fn read_segments(filename: &str) -> Result<Vec<(u64, u64, u64, u64, object::SegmentFlags)>, Box<dyn Error>> {
-    eprintln!("Segments");
-    eprint!("# address size offset length flags");
+    eprint!("Segments");
+    eprintln!("# address size offset length flags");
 
     // Read the object file
     let mut file = File::open(filename)?;
